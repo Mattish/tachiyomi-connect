0055e0cb3ea615e143278aaeb2d686b35896301e
Mattish <mattishd@gmail.com>
2019-08-11 20:46:17 +0100

    Correctly add maincontroller

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using TachiyomiConnect.Dtos;
 
 namespace TachiyomiConnect.Controllers
@@ -94,89 +92,6 @@ namespace TachiyomiConnect.Controllers
     {
         public Dictionary<AccountDevice, Account> AccountDeviceToAccount = new Dictionary<AccountDevice, Account>();
         public Dictionary<Guid, AccountDevice> RecoveryCodeToAccountDevice = new Dictionary<Guid, AccountDevice>();
-    }
-
-    public static class ApplicationStateAccess
-    {
-        private static readonly string DbFileName = "TachiyomiConnect.json";
-        private static readonly object dbLock = new object();
-
-        private static List<TimedAccountCode> timedAccountCodes = new List<TimedAccountCode>();
-
-        public static bool TryGetAccount(AccountDevice accountDevice, out Account account)
-        {
-            lock (dbLock)
-            {
-                var dbState = GetDbState();
-                if (dbState.AccountDeviceToAccount.TryGetValue(accountDevice, out account))
-                {
-                    WriteDbState(dbState);
-                    return true;
-                }
-
-                return false;
-            }
-        }
-
-        public static void RegisterNewAccount(AccountDevice device, Account account, Guid recoveryCode)
-        {
-            lock (dbLock)
-            {
-                var dbState = GetDbState();
-                dbState.AccountDeviceToAccount[device] = account;
-                dbState.RecoveryCodeToAccountDevice[recoveryCode] = device;
-                WriteDbState(dbState);
-            }
-        }
-
-        public static bool TryGetAccountDevice(Guid recoveryCode, out AccountDevice accountDevice)
-        {
-            lock (dbLock)
-            {
-                var dbState = GetDbState();
-                return dbState.RecoveryCodeToAccountDevice.TryGetValue(recoveryCode, out accountDevice);
-            }
-        }
-
-        public static bool TryAddAccountDevice(AccountDevice accountDevice, string accountCode, out TimedAccountCode timedAccountCode)
-        {
-            timedAccountCodes = timedAccountCodes.Where(x => x.ValidUntil > DateTimeOffset.UtcNow).ToList();
-
-            timedAccountCode = timedAccountCodes.FirstOrDefault(x => x.Code == accountCode);
-
-            if (timedAccountCode != null)
-            {
-                lock (dbLock)
-                {
-                    var dbState = GetDbState();
-                    dbState.AccountDeviceToAccount[accountDevice] = timedAccountCode.Account;
-                    timedAccountCode.Account.Devices.Add(accountDevice);
-                    WriteDbState(dbState);
-                    return true;
-                }
-            }
-
-            return false;
-
-        }
-
-        public static void AddTimedAccountCode(TimedAccountCode timedAccountCode)
-        {
-
-        }
-
-        private static TachiyomiDbState GetDbState()
-        {
-            var text = File.ReadAllText(DbFileName);
-            return JsonConvert.DeserializeObject<TachiyomiDbState>(text);
-        }
-
-        private static void WriteDbState(TachiyomiDbState state)
-        {
-            var json = JsonConvert.SerializeObject(state);
-            File.WriteAllText(DbFileName, json);
-        }
-
     }
 
     [Route("")]
